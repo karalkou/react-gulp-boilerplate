a086a341d2c20bfb7497953537e3004f31dfa413
rtivital <rtivital@gmail.com>
2016-03-15 00:27:27 +0300

    Initial commit

---
+++ js/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import Button from './components/Button';
+export default class App extends Component {
+  render() {
+    return (
+      <Button />
+    );
+  }
+}
